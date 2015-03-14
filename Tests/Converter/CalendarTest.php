3e78c7266ab7ba246a173512a6f12a9a4ad687e5
santhapa <san.loggingin@gmail.com>
2015-03-14 18:35:29 +0545

    testing changes

--- Tests/Converter/CalendarTest.php
+++ Tests/Converter/CalendarTest.php
@@ -8,7 +8,7 @@ class CalendarTest extends \PHPUnit_Framework_TestCase
 {
     public function testNepaliToEnglish()
     {
-        $converter = new Calendar();
+        $converter = new Calendars();
         $result = $converter->nepaliToEnglish(2071, 9, 17);
 
         $this->assertEquals('2015-01-01', $result);
